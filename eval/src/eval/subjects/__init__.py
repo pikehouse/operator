7e4dbc2d83f1af24fb7a1bb86c8fe6a050fa10b9
JR Tipton <jr.tipton@gmail.com>
2026-01-29 12:03:53 -0800

    feat(35-02): create eval subjects directory structure
    - Create eval/subjects/ package
    - Create eval/subjects/tikv/ package with __init__.py
    - Ready for TiKVEvalSubject implementation

---
+++ eval/src/eval/subjects/__init__.py
@@ -0,0 +1,1 @@
+"""Evaluation subjects for chaos engineering trials."""
---
+++ eval/src/eval/subjects/tikv/__init__.py
@@ -0,0 +1,5 @@
+"""TiKV evaluation subject."""
+
+from eval.subjects.tikv.subject import TiKVEvalSubject
+
+__all__ = ["TiKVEvalSubject"]
